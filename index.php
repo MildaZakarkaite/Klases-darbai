9fac3068ae96e1ff5d3767de226280f6c90fce29
MildaZakarkaite <54890925+MildaZakarkaite@users.noreply.github.com>
2019-09-05 08:53:18 +0300

    Didžiosios raidės

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$tekstas = 'Tropiniuose kraštuose augantys apelsinai yra ne oranžiniai, o žali. Oranžinę spalvą lemia saulės trūkumas.';
+print $tekstas . '<br><br>';
+
+
+
+$tekstas = 'Tropiniuose kraštuose augantys apelsinai yra ne oranžiniai, o žali. Oranžinę spalvą lemia saulės trūkumas.';
+$tekstas = strtoupper($tekstas);
+print $tekstas; 
+
+?>
